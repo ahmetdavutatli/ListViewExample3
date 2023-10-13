6d16da33ed8c6955c3d6ae7da48f5bcfef7e2bb4
ahmetdavutatli <ahmetdavutatli@posta.mu.edu.tr>
2023-10-13 23:02:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/listviewexample3/ArrayAdapterActivity.java
@@ -0,0 +1,40 @@
+package com.example.listviewexample3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ArrayAdapterActivity extends ListActivity {
+
+
+
+    static final String[] ANIMALS = new String[] { "Ant", "Bear", "Cat", "Dog", "Elephant", "Fox",
+            "Giraffe", "Horse", "Iguana", "Jaguar", "Kangaroo", "Lion", "Monkey", "Owl", "Panda" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.activity_array_adapter);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_array_adapter, ANIMALS));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
